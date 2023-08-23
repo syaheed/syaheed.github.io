51ad496b84c5999a091872eae066d09ab14bfdeb
syaheed <syaheed@gmail.com>
2023-08-23 18:39:05 +0800

    Update vis_depend.R

--- apps/dataVis/vis_depend.R
+++ apps/dataVis/vis_depend.R
@@ -1,3 +1,4 @@
+# Clear all variables
 rm(list = ls())
 
 # Load necessary packages
@@ -38,10 +39,10 @@ p3 = ggplot() +
   theme_minimal() + theme(legend.position = "none")   
 
 p4 = ggplot() + 
-  geom_line(data = summary_data, aes(x = Species, y = mean_slength), group = 1, color = "black", size = 1, linetype = "solid") + 
+  geom_line(data = summary_data, aes(x = Species, y = mean_slength), group = 1, color = "black", linewidth = 1, linetype = "solid") + 
   geom_point(data = summary_data, aes(x = Species, y = mean_slength),shape = 18, size = 3, color = "black") + 
   geom_errorbar(data = summary_data, aes(x = Species, ymin = mean_slength - se_slength, ymax = mean_slength + se_slength),width = 0.2)+
-  geom_line(data = summary_data, aes(x = Species, y = mean_swidth), group = 1, color = "grey", size = 0.5, linetype = "dashed") + 
+  geom_line(data = summary_data, aes(x = Species, y = mean_swidth), group = 1, color = "grey", linewidth = 0.5, linetype = "dashed") + 
   geom_point(data = summary_data, aes(x = Species, y = mean_swidth),shape = 18, size = 3, color = "grey") + 
   geom_errorbar(data = summary_data, aes(x = Species, ymin = mean_swidth - se_swidth, ymax = mean_swidth + se_swidth),color = "grey" ,width = 0.2)+
   labs(title = "Sepal Length and Width by Species", x = "Species", y = "Length/Width") +
@@ -49,3 +50,4 @@ p4 = ggplot() +
 
 x11()
 ggarrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
+ggsave(filename = "vis_depend.jpg",width = 20, height = 20, units = 'cm', dpi = 300)
